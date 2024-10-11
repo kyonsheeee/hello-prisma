e3623f7bdb05a928772bf99b5e7f6b1c523172a0
Kyoko Akatsuka <akatsuka.kyoko@plus-zero.co.jp>
2024-10-11 11:34:05 +0900

    結果の出力

--- script.ts
+++ script.ts
@@ -3,12 +3,8 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const usersWithPosts = await prisma.user.findMany({
-    include: {
-      posts: true,
-    },
-  });
-  console.dir(usersWithPosts, { depth: null });
+  const allUsers = await prisma.user.findMany();
+  console.log(allUsers);
 }
 
 main()
